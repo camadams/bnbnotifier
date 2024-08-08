b0d89df082a65acfee1f473069b7284b04de4285
Cameron Adams <camgadams@gmail.com>
2024-08-08 12:11:04 +0200

    fix: test adding dynamic = force-dynamic fixs /doallurls

--- app/api/doallurls/route.ts
+++ app/api/doallurls/route.ts
@@ -1,7 +1,8 @@
 import { scrapOldestUnprocessedOrSetAllUnprocessedAndTryAgain } from "@/app/dashboard/action";
 import { NextResponse } from "next/server";
 
-export const maxDuration = 30;
+export const maxDuration = 50;
+export const dynamic = "force-dynamic";
 
 export async function GET() {
   const res = await scrapOldestUnprocessedOrSetAllUnprocessedAndTryAgain();
--- app/api/testsendmail/route.ts
+++ app/api/testsendmail/route.ts
@@ -6,12 +6,12 @@ import { Resend } from "resend";
 export async function GET() {
   const resend = new Resend(process.env.RESEND_API_KEY);
 
-  const { data, error } = await resend.emails.send({
-    from: "Acme <onboarding@resend.dev>",
-    to: "camgadams@gmail.com",
-    subject: "Hello world",
-    text: "hji",
-  });
+  // const { data, error } = await resend.emails.send({
+  //   from: "Acme <onboarding@resend.dev>",
+  //   to: "camgadams@gmail.com",
+  //   subject: "Hello world",
+  //   text: "hji",
+  // });
 
   return NextResponse.json("resp", { status: 200 });
 }
